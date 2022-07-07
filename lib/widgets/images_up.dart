a9be1171e0f079005a01202d21723bceaabbb723
Erick Jonathan <e.jonathansandoval200106@gmail.com>
2022-07-07 15:57:42 -0500

    modificaciones

--- lib/widgets/images_up.dart
+++ lib/widgets/images_up.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:event_on_time/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -10,23 +11,28 @@ class ImagesUp extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Stack(
-              children: const [
-                ImagenShow(
+              children: [
+                
+                Padding(
+                  padding: const EdgeInsets.only(left: 50,top: 50),
+                  child: BounceInDown(
+                    duration: Duration(milliseconds: 2000),
+                    child: const ImagenShow(
+                        width: 240,
+                        height: 240,
+                        alignment: Alignment.centerRight,
+                        routeImage: 'assets/images/logo.png'),
+                  ),
+                ),
+                
+                const ImagenShow(
                     width: 200,
                     height: 150,
                     alignment: Alignment.topLeft,
                     routeImage: 'assets/images/cordon.png'),
-                Padding(
-                  padding: EdgeInsets.only(left: 50,top: 30),
-                  child: ImagenShow(
-                      width: 240,
-                      height: 240,
-                      alignment: Alignment.centerRight,
-                      routeImage: 'assets/images/logo.png'),
-                ),
               ],
             ),
           ],
